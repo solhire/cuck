95d00094a09fa857169ed4f764f5f0a0ff0112e1
solhire <solhire@proton.me>
2025-05-07 17:47:42 -0500

    Update SlidingMerch to include cucklong.png

--- src/app/components/SlidingMerch.tsx
+++ src/app/components/SlidingMerch.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const SlidingMerch = () => {
-  const items = ['cuckhood.png', 'cuckt.png'];
+  const items = ['cuckhood.png', 'cucklong.png', 'cuckt.png'];
   
   return (
     <div className="w-screen -mx-4 mt-6 overflow-hidden bg-black/20">
